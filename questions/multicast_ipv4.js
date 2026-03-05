45a9e9cd6c9d4c9971e62bb0e7a1b42819c04d91
dobreeandrei <dobreandrei1234@gmail.com>
2026-03-05 04:27:31 +0200

    combined IPv4 and IPv6 multicast into IP Multicast

--- questions/multicast_ipv4.js
+++ questions/multicast_ipv4.js
@@ -1,6 +1,6 @@
 ACADOBRE_QUESTIONS.push({
   id: 'multicast_ipv4',
-    topic: 'IPv4 Multicast',
+    topic: 'IP Multicast',
     questions: [
       {
         type: 'open-ended',
@@ -41,6 +41,61 @@ ACADOBRE_QUESTIONS.push({
         type: 'open-ended',
         text: 'Care este adresa IPv4 Multicast pentru toate routerele HSRP/GLBP?',
         acceptedAnswers: ['224.0.0.102'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate hosturile?',
+        acceptedAnswers: ['ff02::1'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele?',
+        acceptedAnswers: ['ff02::2'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate servele DHCPv6?',
+        acceptedAnswers: ['ff02::1:2'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele OSPF?',
+        acceptedAnswers: ['ff02::5'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru DR/BDR in OSPF?',
+        acceptedAnswers: ['ff02::6'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele RIP?',
+        acceptedAnswers: ['ff02::9'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele EIGRP?',
+        acceptedAnswers: ['ff02::a'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele VRRP?',
+        acceptedAnswers: ['ff02::12'],
+      },
+      {
+        type: 'open-ended',
+        text: 'Care este adresa IPv6 Multicast pentru toate routerele HSRP/GLBP?',
+        acceptedAnswers: ['ff02::66'],
+      },
+      {
+      type: 'open-ended',
+      text: 'Cati biti are o adresa IPv6?',
+      acceptedAnswers: ['128'],
+      },
+      {
+      type: 'open-ended',
+      text: 'Cati biti are o adresa IPv4?',
+      acceptedAnswers: ['32'],
       },
     ]
 });
